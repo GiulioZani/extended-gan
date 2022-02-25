684893d336ccf0c07e566b34e9465aafac30b56c
GiulioZani <giulio.zani@gmail.com>
2022-02-25 21:40:11 +0100

    bunch of stuff

--- coastal_sea_data_preprocess.py
+++ coastal_sea_data_preprocess.py
@@ -6,15 +6,15 @@ import matplotlib.pyplot as plt
 
 def main():
     acc = []
-    for file_name in ["TEM.nc"]:
-        raw_data = t.from_numpy(netCDF4.Dataset(file_name)["thetao"][...])
+    for var_name, file_name in [("uo", "/mnt/tmp/CUR_uo.nc")]:
+        raw_data = t.from_numpy(netCDF4.Dataset(file_name)[var_name][...])
         subsampled_data = raw_data[:, 0, 20:, :65]
         normalized_data = (subsampled_data - t.min(subsampled_data)) / (
             t.max(subsampled_data) - t.min(subsampled_data)
         )
         acc.append(normalized_data)
     result = t.stack(acc, dim=1)
-    t.save(result, "coastal_sea_data_preprocessed.pt")
+    t.save(result, "coastal_sea_data_preprocessed_cur.pt")
 
 
 if __name__ == "__main__":
