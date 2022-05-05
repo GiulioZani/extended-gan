e5af684d346a36bb5833b040d764e74960548a66
lrhm <ilaimihar@gmail.com>
2022-05-05 13:43:19 +0200

    updates

--- dl/models/axial/model.py
+++ dl/models/axial/model.py
@@ -2,17 +2,18 @@ from ...base_lightning_modules.base_gan_model import GANLightning
 # from ...base_lightning_modules.base_model import BaseRegressionModel
 
 from argparse import Namespace
-
+from ...components.ConvLSTMModule import ConvLSTMClassifier
+from ...components.resnetmodel import ResNetFrameDiscriminator
 from ...components.conv3dmodel import CompactConv3DDiscriminator, Conv3DTemporalDiscriminator
 from ...components.lstmconvmodel import ConvLSTMTemporalDiscriminator
 from ...components.conv2dmodel import CompactFrameDiscriminator, FrameDiscriminator
 from ...components.resnet3d import ResNet3DClassifier
 from ...components.conv3dmodel import Conv3DFrameDiscriminator
-from .modules import AxialGenerator
+from .modules import AxialFrameDiscriminator, AxialGenerator, AxialTemporalDiscriminator
 
 class Model(GANLightning):
     def __init__(self, params: Namespace):
         super().__init__(params)
         self.generator = AxialGenerator(params)
-        self.temporal_discriminator = ResNet3DClassifier(params)
-        self.frame_discriminator = FrameDiscriminator(params)
+        self.temporal_discriminator = ConvLSTMClassifier(params)
+        self.frame_discriminator = ResNetFrameDiscriminator(params)
