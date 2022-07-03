73fe5856c446da580f7a9bdbbb3f41a9cb500f3c
GiulioZani <giulio.zani@gmail.com>
2022-07-03 23:56:00 +0200

    KMNI data loader

--- gan/data_loaders/kmni.py
+++ gan/data_loaders/kmni.py
@@ -1,5 +1,4 @@
 import torch as t
-from torch.utils.data import Dataset
 from pytorch_lightning import LightningDataModule
 import os
 import h5py
@@ -8,62 +7,7 @@ from torch.utils.data import Dataset, DataLoader
 import ipdb
 
 
-class precipitation_maps_h5(Dataset):
-    def __init__(
-        self,
-        in_file,
-        num_input_images,
-        num_output_images,
-        train=True,
-        transform=None,
-    ):
-        super(precipitation_maps_h5, self).__init__()
-
-        self.file_name = in_file
-        self.n_images, self.nx, self.ny = h5py.File(self.file_name, "r")[
-            "train" if train else "test"
-        ]["images"].shape
-
-        self.num_input = num_input_images
-        self.num_output = num_output_images
-        self.sequence_length = num_input_images + num_output_images
-
-        self.train = train
-        # Dataset is all the images
-        self.size_dataset = self.n_images - (
-            num_input_images + num_output_images
-        )
-        # self.size_dataset = int(self.n_images/(num_input_images+num_output_images))
-        self.transform = transform
-        self.dataset = None
-
-    def __getitem__(self, index):
-        # min_feature_range = 0.0
-        # max_feature_range = 1.0
-        # with h5py.File(self.file_name, 'r') as dataFile:
-        #     dataset = dataFile["train" if self.train else "test"]['images'][index:index+self.sequence_length]
-        # load the file here (load as singleton)
-        if self.dataset is None:
-            self.dataset = h5py.File(
-                self.file_name, "r", rdcc_nbytes=1024 ** 3
-            )["train" if self.train else "test"]["images"]
-        imgs = np.array(
-            self.dataset[index : index + self.sequence_length], dtype="float32"
-        )
-
-        # add transforms
-        if self.transform is not None:
-            imgs = self.transform(imgs)
-        input_img = imgs[: self.num_input]
-        target_img = imgs[-1]
-
-        return input_img, target_img
-
-    def __len__(self):
-        return self.size_dataset
-
-
-class precipitation_maps_oversampled_h5(Dataset):
+class CustomDataset(Dataset):
     def __init__(
         self,
         in_file: str,
@@ -71,17 +15,16 @@ class precipitation_maps_oversampled_h5(Dataset):
         num_output_images: int,
         train: bool = True,
         transform=None,
+        crop: int = 40,
     ):
+        self.num_input = num_input_images
+        self.num_output = num_output_images
         self.file_name = in_file
         self.samples, _, _, _ = h5py.File(self.file_name, "r")[
             "train" if train else "test"
         ]["images"].shape
 
-        self.num_input = num_input_images
-        self.num_output = num_output_images
-
         self.train = train
-        # self.size_dataset = int(self.n_images/(num_input_images+num_output_images))
         self.transform = transform
         self.dataset = None
 
@@ -92,17 +35,18 @@ class precipitation_maps_oversampled_h5(Dataset):
                 self.file_name, "r", rdcc_nbytes=1024 ** 3
             )["train" if self.train else "test"]["images"]
         imgs = np.array(self.dataset[index], dtype="float32")
+        print(f"{imgs.shape=}")
         # print("******SHAPE: ", imgs.shape)
         # add transforms
         if self.transform is not None:
             imgs = self.transform(imgs)
-        input_img = imgs[: self.num_input]
+        target_img = imgs[: self.num_output]
         # target_img = imgs[-1] # Modified
-        target_img = imgs[self.num_input :]
+        input_img = imgs[self.num_input :]
 
-        input_img = t.from_numpy(input_img).permute(2, 3, 1, 0)
-        target_img = t.from_numpy(target_img).permute(2, 3, 1, 0)
-        # print(f"{input_img.shape} {target_img.shape}")
+        input_img = t.from_numpy(input_img)[:, None]  # .permute(2, 3, 1, 0)
+        target_img = t.from_numpy(target_img)[:, None]  # .permute(2, 3, 1, 0)
+        print(f"{input_img.shape=} {target_img.shape=}")
 
         return input_img, target_img
 
@@ -112,6 +56,7 @@ class precipitation_maps_oversampled_h5(Dataset):
 
 class CustomDataModule(LightningDataModule):
     def __init__(self, params):
+        super().__init__()
         self.data_location = params.data_location
         self.train_batch_size = params.train_batch_size
         self.test_batch_size = params.test_batch_size
@@ -121,21 +66,30 @@ class CustomDataModule(LightningDataModule):
 
     def train_dataloader(self):
         return DataLoader(
-            precipitation_maps_oversampled_h5(
-                self.data_location, 6, 6, train=True
+            CustomDataset(
+                self.data_location,
+                self.in_seq_len,
+                self.out_seq_len,
+                train=True,
             )
         )
 
     def test_dataloader(self):
         return DataLoader(
-            precipitation_maps_oversampled_h5(
-                self.data_location, 6, 6, train=False
+            CustomDataset(
+                self.data_location,
+                self.in_seq_len,
+                self.out_seq_len,
+                train=False,
             )
         )
 
     def val_dataloader(self):
         return DataLoader(
-            precipitation_maps_oversampled_h5(
-                self.data_location, 6, 6, train=False
+            CustomDataset(
+                self.data_location,
+                self.in_seq_len,
+                self.out_seq_len,
+                train=False,
             )
         )
