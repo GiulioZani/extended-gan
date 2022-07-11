35dbe5b6421cc2496896caae48a670a5a34aed7a
lrhm <ilaimihar@gmail.com>
2022-07-11 23:02:49 +0200

    metrics

--- gan/models/simvp/base_model.py
+++ gan/models/simvp/base_model.py
@@ -23,7 +23,7 @@ class BaseRegressionModel(LightningModule):
         self.mse_metric = torchmetrics.MeanSquaredError()
         self.sum_mse = t.nn.MSELoss(reduction="sum")
 
-        self.denorm_mse = mate.DenormMSE()
+        # self.denorm_mse = mate.DenormMSE()
 
     def forward(self, z: t.Tensor) -> t.Tensor:
         out = self.generator(z)
@@ -44,15 +44,17 @@ class BaseRegressionModel(LightningModule):
                 label="train",
             )
 
+        sum_mse = F.mse_loss(y_pred, y, reduction="sum")
+        self.log("sum_mse", sum_mse, prog_bar=True)
+        return {"loss": loss, "sum_mse": sum_mse}
+
         return {"loss": loss}
 
     def validation_epoch_end(self, outputs):
         avg_loss = t.stack([x["val_loss"] for x in outputs]).mean()
+        avg_sum_mse = t.stack([x["val_sum_mse"] for x in outputs]).mean()
         self.log("val_loss", avg_loss, prog_bar=True)
-
-        denorm_mse = self.denorm_mse.compute()
-        self.denorm_mse.reset()
-        self.log("val_denorm_mse", denorm_mse, prog_bar=True)
+        self.log("val_sum_mse", avg_sum_mse, prog_bar=True)
 
         return {"val_mse": avg_loss}
 
@@ -76,9 +78,8 @@ class BaseRegressionModel(LightningModule):
         loss = F.mse_loss(pred_y, y)
         # sum_mse = self.mse_sum_loss(pred_y, y)
         # self.log("val_mse", loss, prog_bar=True)
-        self.denorm_mse.update(y, pred_y)
-
-        return {"val_mse": loss, "val_loss": loss}
+        sum_mse = F.mse_loss(pred_y, y, reduction="sum")
+        return {"val_mse": loss, "val_loss": loss, "val_sum_mse": sum_mse}
 
     def test_step(self, batch: tuple[t.Tensor, t.Tensor], batch_idx: int):
         x, y = batch
@@ -95,19 +96,16 @@ class BaseRegressionModel(LightningModule):
         pred_y = self(x)
         # se = F.mse_loss(pred_y, y, reduction="sum")
         self.mse_metric.update(y, pred_y)
-        self.denorm_mse.update(y, pred_y)
+        sum_mse = F.mse_loss(pred_y, y, reduction="sum")
+        return {"test_mse": self.mse_metric.compute(), "test_sum_mse": sum_mse}
 
     def test_epoch_end(self, outputs):
-
+        avg_test_mse = t.stack([x["test_sum_mse"] for x in outputs]).mean()
         loss = self.mse_metric.compute()
         self.log("test_mse", loss, prog_bar=True)
         self.mse_metric.reset()
-
-        denorm_mse = self.denorm_mse.compute()
-        self.denorm_mse.reset()
-        self.log("test_denorm_mse", denorm_mse, prog_bar=True)
-
-        return {}
+        self.log("test_sum_mse", avg_test_mse, prog_bar=True)
+        return {"test_mse": loss}
 
     def configure_optimizers(self):
         # lr = self.params.lr
